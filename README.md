f3ea55e0dc9d9d6a5a0d1898fc1f4d14e63a737a
shikazuki <shikazuki0@gmail.com>
2021-01-30 22:33:03 +0900

    setup replication.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # postgresql-replication
+This repository validates postgres replication by using docker. 
+
+## How
+1. Setup primary db.
+```
+$ docker-compose up -d primary
+```
+2. Store basebackup from primary db.
+```
+$ docker-compose run -u postgres secondary /backup/setup-db.sh
+```
+
+3. Run standby db.
+```
+$ docker-compose up -d secondary
+```
--- secondary/setup-db.sh
+++ secondary/setup-db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-cp /docker-entrypoint-initdb.d/pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
-cp /docker-entrypoint-initdb.d/postgresql.conf /var/lib/postgresql/data/postgresql.conf
+pg_basebackup -h 192.168.160.2 -U admin -D /var/lib/postgresql/data/ -R --progress
+sed -i.bak -e 's/#\?hot_standby = on/hot_standby = on/g' /var/lib/postgresql/data/postgresql.conf
