7bf943470cc100ce3040563aa490c395ae4dec89
shikazuki <shikazuki0@gmail.com>
2021-01-30 12:04:45 +0900

    setup postgres by using docker.

---
+++ primary/setup-db.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+
+cp /docker-entrypoint-initdb.d/pg_hba.conf /var/lib/postgresql/data/pg_hba.conf
+cp /docker-entrypoint-initdb.d/postgresql.conf /var/lib/postgresql/data/postgresql.conf
